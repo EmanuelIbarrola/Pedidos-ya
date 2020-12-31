d17461bfc1ccef50ef5f4f62b60379bbb5d61914
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2020-12-31 16:22:04 -0300

    se agrego el scrip de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
-import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
-import {MatIconModule} from '@angular/material/icon';
 import { RoutingRoutingModule } from './routing/routing-routing.module';
 
 
@@ -20,11 +15,7 @@ import { RoutingRoutingModule } from './routing/routing-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RoutingRoutingModule,
-
-    MatSliderModule,
-    MatIconModule,
-
+    RoutingRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
