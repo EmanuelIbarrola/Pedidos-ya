b965447455ed895d3abceb77e848f0e525d97265
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2020-12-31 15:15:36 -0300

    Se agrego la parte del nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import { RoutingRoutingModule } from './routing/routing-routing.module';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RoutingRoutingModule,
+
+    MatSliderModule,
+    MatIconModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
