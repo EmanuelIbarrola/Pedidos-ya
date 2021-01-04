152859a5241d5e408161e6bcd435280cf5db5085
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2021-01-04 15:53:55 -0300

    home listado de comidas

--- src/app/pages/home/Components/home/home.component.ts
+++ src/app/pages/home/Components/home/home.component.ts
@@ -7,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  productos=[
+    {
+      titulo:"Pizza",
+      descripcion:"ricas",
+      precio: 220,
+      id:1,
+      imageUrl:"ada"
+    },
+
+    {
+      titulo:"Pizza2",
+      descripcion:"ricas",
+      precio: 220,
+      id:2,
+      imageUrl:"ada"
+    },
+
+    {
+      titulo:"Pizza3",
+      descripcion:"ricas",
+      precio: 220,
+      id:3,
+      imageUrl:"ada"
+    },
+    {
+      titulo:"Pizza4",
+      descripcion:"ricas",
+      precio: 220,
+      id:4,
+      imageUrl:"ada"
+    },
+    {
+      titulo:"Pizza5",
+      descripcion:"ricas",
+      precio: 220,
+      id:5,
+      imageUrl:"ada"
+    },
+    {
+      titulo:"Pizza6",
+      descripcion:"ricas",
+      precio: 220,
+      id:6,
+      imageUrl:"ada"
+    },
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
