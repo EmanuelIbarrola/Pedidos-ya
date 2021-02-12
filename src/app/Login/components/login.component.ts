36d98e02a482e1c2ddb0d029c451e7ba6b10412b
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2021-02-12 17:33:50 -0300

    Login con autentificacion

--- src/app/Login/components/login.component.ts
+++ src/app/Login/components/login.component.ts
@@ -1,43 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutentificaciónService } from 'src/app/Services/autentificación.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
-  form!: FormGroup;
 
-  constructor(private formbuilder: FormBuilder) {
-    this.buildForm();
-  }
+  constructor(private router: Router, private autentificaciónService:AutentificaciónService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  
+  onlogin(form: any) {
+    console.log("FORM: ",form.value)
+    this.autentificaciónService.Login(form.value).subscribe( res =>{
+      console.log("Login Response: ", res)
+    },
+    error=>{
+      console.log("Login Error: ",error)
 
-  }
-  private buildForm() {
-    this.form =this.formbuilder.group({
-      Email:['',[Validators.required,Validators.email]],
-      pass: ['', [Validators.required, Validators.minLength(4)]],
-     });
-  }
-
-  save() {
-    if(this.form.valid){
-      console.log(  this.form.value);
-    }else{
-      this.form.markAllAsTouched();
     }
- }
 
- get emailCampo(){
-   return this.form.get('Email');
- }
+    )
 
- get passCampo(){
-  return this.form.get('pass');
-}
+  }
 
 }
--- src/app/Login/login/login.module.ts
+++ src/app/Login/login/login.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,17 +6,23 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from '../components/login.component';
 import { NgElseDirective } from 'src/app/Directivas/ng-else.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AutentificaciónService } from 'src/app/Services/autentificación.service';
+
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     NgElseDirective,
+   
   ],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    ReactiveFormsModule
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers:[ AutentificaciónService],
 })
 export class LoginModule { }
