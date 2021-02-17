f5b3f65c5fdc5505b5b9856d8f1fd3680be4123a
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2021-02-17 02:06:36 -0300

    guards implementado a la ruta pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './shared/guards/auth.guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -9,7 +10,11 @@ import { AuthInterceptor } from './shared/Interceptores/auth.interceptor';
 const routes: Routes = [
   {path:"" , redirectTo:"login", pathMatch:"full"},
   {path:"login", loadChildren: ()=> import("../app/Login/login.module").then(m=>m.LoginModule)},
-  {path:"pages", loadChildren:()=> import("./Pages/pages.module").then(m=>m.PagesModule)},
+  {
+    path:"pages", loadChildren:()=> import("./Pages/pages.module").then(m=>m.PagesModule),
+    canActivate:[AuthGuard]
+  
+  },
 
 
 ];
@@ -28,6 +33,7 @@ const routes: Routes = [
 
   ],
   providers: [
+    AuthGuard,
     {
       provide:HTTP_INTERCEPTORS,
       useClass:AuthInterceptor,
