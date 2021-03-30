377c97bcb67b24f3448158e8bc4a498ce03fda41
EmanuelIbarrola <ema_ibarrola@hotmail.com>
2021-03-30 14:31:24 -0300

    mejorando el login

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -8,13 +8,20 @@ import { AutentificaciónService } from 'src/app/shared/Services/autentificació
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
+   login: Boolean = true
 
   constructor(private router: Router, private autentificaciónService:AutentificaciónService) {}
     LoginError=""
   ngOnInit(): void {
     if(this.autentificaciónService.verificacionLogueado())
     this.router.navigate(['pages']);
+  }
+
+  changeForm(){
+    this.login = !this.login;
+  }
+  onRegister(form:any){
+    console.log(" registro ")
   }
 
   onlogin(form: any) {
